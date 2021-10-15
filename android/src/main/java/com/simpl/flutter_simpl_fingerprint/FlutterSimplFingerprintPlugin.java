15597773fa2e258abba2692286aae9f8821a0214
Ajaykkumar <ajay.kumar@simpl-pay.com>
2021-10-15 17:22:41 +0530

    Simpl-android-sdk-integration

--- android/src/main/java/com/simpl/flutter_simpl_fingerprint/FlutterSimplFingerprintPlugin.java
+++ android/src/main/java/com/simpl/flutter_simpl_fingerprint/FlutterSimplFingerprintPlugin.java
@@ -1,5 +1,6 @@
 package com.simpl.flutter_simpl_fingerprint;
 
+import android.content.Context;
 import androidx.annotation.NonNull;
 
 import io.flutter.embedding.engine.plugins.FlutterPlugin;
@@ -8,25 +9,30 @@ import io.flutter.plugin.common.MethodChannel;
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler;
 import io.flutter.plugin.common.MethodChannel.Result;
 import io.flutter.plugin.common.PluginRegistry.Registrar;
+import com.simpl.android.fingerprint.SimplFingerprint;
+import com.simpl.android.fingerprint.SimplFingerprintListener;
+
+import java.util.Map;
 
 /** FlutterSimplFingerprintPlugin */
 public class FlutterSimplFingerprintPlugin implements FlutterPlugin, MethodCallHandler {
-  /// The MethodChannel that will the communication between Flutter and native Android
-  ///
-  /// This local reference serves to register the plugin with the Flutter Engine and unregister it
-  /// when the Flutter Engine is detached from the Activity
   private MethodChannel channel;
+
+  private Context context;
 
   @Override
   public void onAttachedToEngine(@NonNull FlutterPluginBinding flutterPluginBinding) {
     channel = new MethodChannel(flutterPluginBinding.getBinaryMessenger(), "flutter_simpl_fingerprint");
     channel.setMethodCallHandler(this);
+    context = flutterPluginBinding.applicationContext;
   }
 
   @Override
   public void onMethodCall(@NonNull MethodCall call, @NonNull Result result) {
-    if (call.method.equals("getPlatformVersion")) {
-      result.success("Android " + android.os.Build.VERSION.RELEASE);
+    if (call.method.equals("generateFingerprint")) {
+      Map<String, String> args = (Map<String, String>)call.arguments;
+      String fp = generateFingerprint(args.get("phoneNumber"), args.get("emailId"));
+      result.success(fp);
     } else {
       result.notImplemented();
     }
@@ -35,5 +41,17 @@ public class FlutterSimplFingerprintPlugin implements FlutterPlugin, MethodCallH
   @Override
   public void onDetachedFromEngine(@NonNull FlutterPluginBinding binding) {
     channel.setMethodCallHandler(null);
+  }
+
+  public String generateFingerprint(final String phoneNumber, final String emailId) {
+    SimplFingerprint.init(context, phoneNumber, emailId);
+    final String[] fingerprint = new String[1];
+    SimplFingerprint.getInstance().generateFingerprint(new SimplFingerprintListener() {
+      @Override
+      public void fingerprintData(String fp) {
+        fingerprint[0] = fp;
+      }
+    });
+    return fingerprint[0];
   }
 }
